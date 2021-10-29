5c2ac42d634dbe8a86415adfe4b11a40435dea5b
ian038 <ianphua3@gmail.com>
2021-10-29 14:52:27 -0400

    WIP

--- src/App.svelte
+++ src/App.svelte
@@ -1,55 +1,7 @@
 <script>
-	let firstName = 'Ian';
-	let lastName = 'Phua'
-	let showText = false
-	let users = [
-		{
-			id: '1',
-			name: 'John'
-		},
-		{
-			id: '2',
-			name: 'Brandon'
-		},
-		{
-			id: '3',
-			name: 'Mike'
-		}
-	]
 
-	const toggle = () => {
-		showText = !showText
-	}
-
-	$: name = firstName + ' ' + lastName
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	{#if showText}
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	{/if}
-	<button on:click={toggle}>Click</button>
+	<h1>Hello</h1>
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
