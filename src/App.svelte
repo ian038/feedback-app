e5283bec1ecce3c4d934c0f21bd9ae84fcec1e13
ian038 <ianphua3@gmail.com>
2021-11-01 13:14:34 -0400

    Created components for feedback lists

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,29 @@
 <script>
+	import FeedbackList from "./components/FeedbackList.svelte";
 
+	const deleteFeedback = e => {
+		console.log(e.detail)
+	}
+
+	const feedback = [
+		{
+			id: 1,
+			rating: 10,
+			text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+		},
+		{
+			id: 2,
+			rating: 9,
+			text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+		},
+		{
+			id: 3,
+			rating: 8,
+			text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+		}
+	]
 </script>
 
-<main>
-	<h1>Hello</h1>
+<main class='container'>
+	<FeedbackList {feedback} on:delete-feedback={deleteFeedback} />
 </main>
